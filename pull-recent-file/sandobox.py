c2b169f913e90ee1f5d6106dcd27788d58f15674
hellojameslee7 <65960087+hellojameslee7@users.noreply.github.com>
2021-04-17 10:55:31 -0500

    Update sandobox.py

--- pull-recent-file/sandobox.py
+++ pull-recent-file/sandobox.py
@@ -1,4 +1,4 @@
-# Pull latest file using the date details from the file name
+# Goal of this: Come up with script that will pull latest file using the date details from the file name
 
 # Steps that I will take:
 # Based on the type of file extract the date string from the filenames.
@@ -9,6 +9,15 @@
 #Goal of this project: Come up with a function that can pull the latest file from a list of file based on some parameters provided to the function
 
 #Got help from this stackoverflow site: https://stackoverflow.com/questions/21184018/get-latest-file-based-on-filename-python
+#Also this site was v helpful for testing reg ex: https://regex101.com/
+#Thanks googs: https://developers.google.com/edu/python/regular-expressions
+
+"""
+Some additional notes for self:
+re.match always starts at the beginning of the string.
+re.search steps through the string from the start looking for the first match. It stops when it finds a match.
+re.findall returns a list of all the search matches.
+"""
 
 filenames = [
     'RandomStuffReport202104051119093gmawt.csv',
@@ -17,17 +26,23 @@ filenames = [
     'Random_Stuff_Report_2021-04-05T0905_3gmawt.csv',
     'Random_Stuff_Report_2021-04-11T0905_3gmawt.csv',
     'Random_Stuff_Report_2021-04-12T0905_3gmawt.csv',
-    'Another Report_Test-12-16-2013.10_52-en.zip',
+    'Random_Stuff_Report_2021-05-12T0905_3gmawt.csv',
+    'Random_Stuff_Report_2021-06-12T0905_3gmawt.csv',
+    'Another Report_Test-20131216.10_52-en.csv',
+    'Another Report_Test-20141216.10_52-en.csv',
+    'Another Report_Test-20141217.10_52-en.csv'
 ] # Used in place of `os.listdir(....)`
 
 import re
 import datetime
 
-date_pattern = re.compile(r'\b(\d{4})-(\d{2})-(\d{2})\b')
+date_pattern = re.compile(r'(\d{4})-(\d{2})-(\d{2})')
+alt_date_pattern = re.compile(r'(\d{4})(\d{2})(\d{2})')
+
 def get_date(filename):
     matched = date_pattern.search(filename)
     if not matched:
-        return None
+        matched = alt_date_pattern.search(filename)
     y, m, d = map(int, matched.groups())
     return datetime.date(y, m, d)
 
